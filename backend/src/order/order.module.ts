c14fe626942fae733ddddc1848d5c2391dbdad4c
Sergei74660 <sergei74660@gmail.com>
2026-07-15 23:35:19 +0300

    feat: migrate from MongoDB to PostgreSQL

--- backend/src/order/order.module.ts
+++ backend/src/order/order.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
-import { FilmsRepository } from 'src/repository/films.repository';
-import { Film, FilmSchema } from 'src/films/films.schema';
+import { FilmsModule } from 'src/films/films.module';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Film.name, schema: FilmSchema }]),
-  ],
+  imports: [FilmsModule],
   controllers: [OrderController],
-  providers: [OrderService, FilmsRepository],
+  providers: [OrderService],
 })
 export class OrderModule {}
